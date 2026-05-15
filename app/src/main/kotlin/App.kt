32170534cc613be639e8b869a09bb680c5a9957f
Victor Baxan <vbaxan.linkedin@gmail.com>
2026-05-15 21:59:26 +0300

    codecrafters submit [skip ci]

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -4,12 +4,25 @@ fun main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println("Logs from your program will appear here!")
 
-    var serverSocket = ServerSocket(4221)
+    ServerSocket(4221).use { server ->
+        // Since the tester restarts your program quite often, setting SO_REUSEADDR
+        // ensures that we don't run into 'Address already in use' errors
+        server.reuseAddress = true
 
-     // Since the tester restarts your program quite often, setting SO_REUSEADDR
-     // ensures that we don't run into 'Address already in use' errors
-     serverSocket.reuseAddress = true
+        while (true) {
+            val client = server.accept()
 
-     serverSocket.accept() // Wait for connection from client.
-     println("accepted new connection")
+            Thread {
+                client.use { socket ->
+                    val writer = socket.getOutputStream().bufferedWriter()
+                    val message = "HTTP/1.1 200 OK\r\n\r\n"
+
+                    writer.run {
+                        write(message)
+                        flush()
+                    }
+                }
+            }.start()
+        }
+    }
 }
